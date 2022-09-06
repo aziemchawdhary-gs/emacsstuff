08a8b9c06dc5e3610f5a78ff1ccc5c53020ec92b
Aziem Chawdhary <aziem.chawdhary@gmail.com>
2022-09-06 10:14:01 +0100

    Add Java config

--- init.el
+++ init.el
@@ -3,11 +3,12 @@
 (setq read-process-output-max (* 1024 1024))
 
 (require 'package)
-
-
-(add-to-list 'package-archives
-               '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (package-initialize)
+(package-refresh-contents)
+(package-install 'use-package)
+(setq use-package-always-ensure t)
+(require 'use-package)
 
 (require 'cc-mode)
 
@@ -21,19 +22,56 @@
 
 (package-install 'evil)
 (package-install 'js2-mode)
-(package-install 'helm)
 
 (use-package projectile :ensure t)
 (use-package yasnippet :ensure t)
 (use-package hydra :ensure t)
 
+(use-package lsp-mode :hook ((lsp-mode . lsp-enable-which-key-integration))
+  :config (setq lsp-completion-enable-additional-text-edit nil))
+(use-package hydra)
+(use-package company)
+(use-package lsp-ui)
+(use-package which-key :config (which-key-mode))
+(use-package lsp-java :config (add-hook 'java-mode-hook 'lsp))
+(use-package dap-mode :after lsp-mode :config (dap-auto-configure-mode))
+(use-package dap-java :ensure nil)
+(use-package helm-lsp)
+(use-package helm
+  :config (helm-mode))
+(use-package lsp-treemacs)
+(use-package projectile
+    :ensure t
+    :init (projectile-mode +1)
+    :config
+    (define-key
+        projectile-mode-map
+        (kbd "C-c p")
+      'projectile-command-map))
+
 (global-set-key (kbd "C-x m") 'eshell)
+(use-package magit)
+
+(add-hook 'compilation-filter-hook
+          (lambda () (ansi-color-apply-on-region (point-min) (point-max))))
+
+(use-package lsp-mode
+    :bind
+    (:map lsp-mode-map
+          (("\C-\M-b" . lsp-find-implementation)
+           ("M-RET" . lsp-execute-code-action))))
+
+(use-package dap-java
+  :ensure nil
+  :after (lsp-java)
+
+  :config
+  (global-set-key (kbd "<f7>") 'dap-step-in)
+  (global-set-key (kbd "<f8>") 'dap-next)
+  (global-set-key (kbd "<f9>") 'dap-continue))  
 
 (require 'evil)
 (evil-mode 1)
-
-(require 'helm-config)
-(helm-mode 1)
 
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x b") 'helm-mini)
